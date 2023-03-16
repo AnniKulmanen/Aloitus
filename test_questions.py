53a0770335dbefa448520ca8e65e147c01023608
AnniK <122267866+annikul@users.noreply.github.com>
2023-03-16 19:21:06 +0200

    Luotu testit

--- test_questions.py
+++ test_questions.py
@@ -7,62 +7,52 @@ import questions
 def test_ask_user_integer(monkeypatch): # Simulate user input using Monkeypathc library
     user_input = '100'
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_integer('Anna kokonaisluku: ', False) == (
-        100, 'OK', 0, 'Conversion successful') 
+    question = questions.Question('Anna kokonaisluku')
+    assert question.ask_user_integer(False) == (100, 'OK', 0, 'Conversion successful') 
     
 # Test if conversion to float works as expected
 def test_ask_user_float(monkeypatch): 
     user_input = '1.5'
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_float('Anna liukuluku: ', False) == (
-        1.5, 'OK', 0, 'Conversion successful') 
+    question = questions.Question('Anna liukuluku') 
+    assert question.ask_user_float(False) == (1.5, 'OK', 0, 'Conversion successful') 
 
 # Test an error condition when user adds a unit to a floating point number
 def test_ask_user_float2(monkeypatch):
-    user_input = '1.5'
+    user_input = '1.5v'
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_float('Anna kokonaisluku:', False) ==(
-        0, 'Error', 1, "could not convert string to float: '1.5v'")
+    question = questions.Question('Anna kokonaisluku') 
+    assert question.ask_user_float(False) == (0, 'Error', 1, "could not convert string to float: '1.5v'")
+    
+# Test an error condition when users uses comma instead of dot as decimal separator
+def test_ask_user_float3(monkeypatch):
+    user_input = '74,6'
+    # Use anonymous function to create input from variable
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
+    question = questions.Question('Anna kokonaisluku')
+    assert question.ask_user_float(False) == (0, 'Error', 1, "could not convert string to float: '74,6'")
 
 # Test conversion to boolean: case Y
-def test_ask_user_boolean(monkeypatch):
+def test_ask_user_boolean(monkeypatch): 
     user_input = 'y'
-    # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_boolean('Haluatko jatkaa', 'Y', 'N', False) == (
-        True, 'OK', 0, 'Conversion successful')
+    question = questions.Question('Haluatko jatkaa') 
+    assert question.ask_user_boolean('Y', 'N', False) == (True, 'OK', 0, 'Conversion successful')
 
 # Test conversion to boolean: case N
-# Simulate user input using Monkeypatch library
-def test_ask_user_boolean2(monkeypatch):
+def test_ask_user_boolean2(monkeypatch): 
     user_input = 'n'
-    # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_boolean('Haluatko jatkaa', 'Y', 'N', False) == (
-        False, 'OK', 0 'Conversion successfel')
+    question = questions.Question('Haluatko jatkaa') 
+    assert question.ask_user_boolean('Y', 'N', False) == (False, 'OK', 0, 'Conversion successful')
 
 # Test an error condition
-def test_ask_user_boolean3(monkeypatch):
+def test_ask_user_boolean3(monkeypatch): 
     user_input = 'v'
-    # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_boolean('Haluatko jatkaa', 'Y', 'N', False) == (
-        'N/A', 'Error', 1 'unable to convert to boolean')
+    question = questions.Question('Haluatko jatkaa') 
+    assert question.ask_user_boolean('Y', 'N', False) == ('N/A', 'Error', 1, 'unable to convert to boolean')
 
-# Test to get value from dictionary
-def test_ask_user_dictionary(monkeypatch):
-    user_input = 'tyttö'
-    gender_dictionary = {'tyttö': 0, 'poika': 1}
-    monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_dictionary('Sukupuoli: ', gender_dictionary, False) == (
-        0, 'OK', 0, 'Conversion successfull')
-
-# Test to get error when key is missing
-def test_ask_user_boolean2(monkeypatch):
-    user_input = 'jätkä'
-    gender_dictionary = {'tyttö': 0, 'poika': 1}
-    monkeypatch.setattr('builtins.input', lambda _: user_input)
-    assert questions.Question.ask_user_dictionary('Sukupuoli: ', gender_dictionary, False) == (
-        'N/A', 'Error', 1, "'jätkä'")
+# Change methods to static or class methods and test again 
